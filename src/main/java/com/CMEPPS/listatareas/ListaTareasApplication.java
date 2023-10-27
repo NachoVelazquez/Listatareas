c6b1aa7825b4a0b10458453161bc5dc05684bb8a
nacho <nacho@DESKTOP-S4QGEUA>
2023-10-27 17:27:05 +0200

    Errores solucionados

--- src/main/java/com/CMEPPS/listatareas/ListaTareasApplication.java
+++ src/main/java/com/CMEPPS/listatareas/ListaTareasApplication.java
@@ -1,4 +1,4 @@
-package com.CMEPPS.lista.tareas;
+package com.CMEPPS.listatareas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
