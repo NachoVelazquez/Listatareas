34cd70260116912996bd4ddf9a5c252eeb1d6d6b
nacho <nacho@DESKTOP-S4QGEUA>
2023-10-27 16:30:59 +0200

    primer commit

---
+++ src/test/java/com/CMEPPS/lista/tareas/ListaTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CMEPPS.lista.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
